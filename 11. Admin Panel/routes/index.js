6404f698d5b0b628a479a6596a0c9c46eb51e238
Abhijit1326 <abhijitsonkamble01801@gmail.com>
2026-01-11 20:10:39 +0530

    Panel

--- 11. Admin Panel/routes/index.js
+++ 11. Admin Panel/routes/index.js
@@ -1,5 +1,3 @@
-
-
 const express = require ("express");
 const { dashboardPage } = require("../controllers/admin.controller");
 
--- 11. Admin Panel/server.js
+++ 11. Admin Panel/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 1000;
+
+app.set("view engine" , "ejs");
+app.use(express.static(path.join(__dirname , "public")));
+app.use("/" ,require("./routes/index")); //Route la server sobt attach karla
 
 app.listen(PORT , (err)=>{
     if (err) {
